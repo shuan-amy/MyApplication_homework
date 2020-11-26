8080194b511eea1ca2c9f778db40a1601368b4b0
shuan-amy <aiproject523@gmail.com>
2020-11-27 00:25:17 +0800

    自動切換啟動畫面

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,9 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -30,17 +33,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-
-    //public MainActivity() {
-    //    super();
-    //}
-
     @Override
     protected void onStart() {
         super.onStart();
         // 找到view root，然後設定click監聽
-        findViewById(android.R.id.content).setOnClickListener(this);
+        //findViewById(android.R.id.content).setOnClickListener(this);
         Log.d(TAG, "enter onStart(). #" + count );
+
+        //使用Handler自動切換螢幕
+        Message msg = myHandler.obtainMessage(); // 從Message pool裡面取一個message出來
+                                                // 比新建立一個有效率
+                                                // Ctr+Q 查看指令文件
+        myHandler.sendMessageDelayed(msg,500); // 0.5秒後執行
+
     }
 
     @Override
@@ -91,6 +96,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         }
     }
+
+    //宣告一個Handler物件，並且連結到UI Thread
+    private Handler myHandler = new Handler(Looper.getMainLooper()){
+
+        @Override
+        public void handleMessage(Message msg){
+            //這裡不處理msg內容，所以收到任何msg都是執行Activity切換的工作
+            startActivity(new Intent(MainActivity.this,Bookkeeping.class));
+
+            //增加過場動畫
+            overridePendingTransition(android.R.anim.slide_in_left,
+                    android.R.anim.slide_out_right);
+            MainActivity.this.finish();
+
+        }
+
+
+    };
+
+
+
+
+
 }
 
 
